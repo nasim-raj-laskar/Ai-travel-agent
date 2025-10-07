ab28f461ddaa2e76f8bf54a382ee04e89c712f7a
NASIM <nosytub@gmail.com>
2025-10-07 20:14:56 +0530

    LLM Chain Updated

---
+++ src/chains/itinerary_chain.py
@@ -0,0 +1,34 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from src.config.config import GROQ_API_KEY
+
+llm=ChatGroq(
+    groq_api_key=GROQ_API_KEY,
+    model_name="llama-3.3-70b-versatile",
+    temperature=0.3,
+)
+
+itineary_prompt=ChatPromptTemplate([
+    ("system" , "You are a helpful travel asssistant. Create a day trip itineary for {city} based on user's interest : {interests}. Provide a brief , bulleted itineary"),
+    ("human" , "Create a itineary for my day trip")
+])
+
+def generate_itineary(city:str, interests:list[str])-> str:
+    reponse=llm.invoke(
+        itineary_prompt.format_messages(city=city, interests=', '.join(interests))
+    )
+    return reponse.content
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
